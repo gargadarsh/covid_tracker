cdb6ac0624411423b5e4f74a45c2d79d6343a77b
Adarsh Garg <gargadarsh10@gmail.com>
2020-08-23 21:18:42 -0700

    Added pull API functionality

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import json
 import pyttsx3
 import speech_recognition as sr
 import re
-import config
 
+import threading
+from time import sleep
+
+import config
 
 
 class Data:
@@ -14,12 +17,13 @@ class Data:
         self.params = {
             "api_key": self.api_key
         }
-        self.getData()
+        self.data = self.getData()
 
     def getData(self):
-        response = requests.get(f'https://www.parsehub.com/api/v2/projects/{config.PROJECT_TOKEN}/last_ready_run/data',
-                                params={"api_key": config.API_KEY})
-        self.data = json.loads(response.text)
+        response = requests.get(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/last_ready_run/data',
+                                params=self.params)
+        data = json.loads(response.text)
+        return data
 
     def getTotalCases(self):
         data = self.data['total']
@@ -56,6 +60,27 @@ class Data:
             countries.append(country['name'].lower())
         return countries
 
+    def updateData(self):
+        response = requests.post(f'https://www.parsehub.com/api/v2/projects/{config.PROJECT_TOKEN}/run',
+                                params=self.params)
+        oldData = self.data
+
+        def pull():
+            sleep(0.1)
+            check = True
+            while check:
+                newData = self.getData()
+                if newData != oldData:
+                    self.data = newData
+                    print("Data Updated!")
+                    check = False
+                sleep(5)
+
+
+        thread = threading.Thread(target=pull)
+        thread.start()
+
+
 
 
 def speak(text):
@@ -82,9 +107,10 @@ def main():
     print("NOTE: Say STOP to stop the program!")
 
     data = Data(config.API_KEY, config.PROJECT_TOKEN)
+    countries = data.getListOfCountries()
+
 
     #potential phrases
-    END = 'STOP'
     TOTAL_PATT = {
         re.compile("[\w\s]+ total [\w\s]+ cases"): data.getTotalCases,
         re.compile("[\w\s]+ total cases "): data.getTotalCases,
@@ -103,8 +129,14 @@ def main():
     }
 
     COUNTRY_PATT = {
-        re.compile("[\w\s]+ cases [\w\s]+"): lambda country: data.getCountryData(country)['total_cases']
+        re.compile("[\w\s]+ cases [\w\s]+"): lambda country: data.getCountryData(country)['total_cases'],
+        re.compile("[\w\s]+ deaths [\w\s]+"): lambda country: data.getCountryData(country)['total_deaths'],
+
+        re.compile("[\w\s]+ recovered [\w\s]+"): lambda country: data.getCountryData(country)['total_recoveries'],
+        re.compile("[\w\s]+ recoveries [\w\s]+"): lambda country: data.getCountryData(country)['total_recoveries']
     }
+
+    UPDATE_PATT = "update"
 
 
     while True:
@@ -112,15 +144,39 @@ def main():
         text = getAudio()
         result = None
 
+
+        #for outputting country specific data
+        for pattern, func in COUNTRY_PATT.items():
+            if pattern.match(text):
+                words = list(text.split(" "))
+                for country in countries:
+                    if country in words:
+                        result = func(country)
+                        break
+
+
+        #for outputting total
         for pattern, func in TOTAL_PATT.items():
             if pattern.match(text):
                 result = func()
                 break
 
+        #to update data
+        if text == UPDATE_PATT:
+            print("Data is being updated... This may take a while...")
+            speak("Data is being updated... This may take a while...")
+            data.updateData()
+            print("Data is up to date!")
+            speak("Data is up to date")
+
+
+
+
         if result:
             speak(result)
 
-        if text.find(END) != -1:
+        if text.find("stop") != -1:
+            print("Program has stopped")
             break
 
 main()
